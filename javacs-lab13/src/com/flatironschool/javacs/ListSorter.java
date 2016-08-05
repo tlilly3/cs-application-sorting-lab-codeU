7852811675d043717e525d74c131e8e6bb5f2b1b
Thomas Lilly <tlilly3@gatech.edu>
2016-08-05 17:53:28 -0400

    Done

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.flatironschool.javacs;
 
@@ -18,13 +18,13 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void insertionSort(List<T> list, Comparator<T> comparator) {
-	
+
 		for (int i=1; i < list.size(); i++) {
 			T elt_i = list.get(i);
 			int j = i;
@@ -42,7 +42,7 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
@@ -55,58 +55,87 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * Returns a list that might be new.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+		if (list.size() == 1) {
+			return list;
+		} else {
+			List<T> fh = new LinkedList<>(mergeSort(list.subList(0, list.size()/2), comparator));
+        	List<T> lh = new LinkedList<>(mergeSort(list.subList(list.size()/2, list.size()), comparator));
+        	List<T> full = new LinkedList<>();
+
+        	while (!fh.isEmpty() && !lh.isEmpty()) {
+        		if (comparator.compare(fh.get(0), lh.get(0)) < 0
+        			|| comparator.compare(fh.get(0), lh.get(0)) == 0) {
+        			full.add(fh.remove(0));
+        		} else if (comparator.compare(fh.get(0), lh.get(0)) > 0) {
+        			full.add(lh.remove(0));
+        		}
+        	}
+			while (!fh.isEmpty()) {
+				full.add(fh.remove(0));
+			}
+			while (!lh.isEmpty()) {
+				full.add(lh.remove(0));
+			}
+			return full;
+		}
+
 	}
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+        PriorityQueue<T> pq = new PriorityQueue<>(list.size(), comparator);
+        pq.addAll(list);
+        list.clear();
+        T ele = pq.poll();
+        while(ele != null) {
+        	list.add(ele);
+        	ele = pq.poll();
+        }
 	}
 
-	
+
 	/**
 	 * Returns the largest `k` elements in `list` in ascending order.
-	 * 
+	 *
 	 * @param k
 	 * @param list
 	 * @param comparator
-	 * @return 
+	 * @return
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        heapSort(list, comparator);
+        return new LinkedList<>(list.subList(list.size()-k, list.size()));
 	}
 
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
-		
+
 		Comparator<Integer> comparator = new Comparator<Integer>() {
 			@Override
 			public int compare(Integer n, Integer m) {
 				return n.compareTo(m);
 			}
 		};
-		
+
 		ListSorter<Integer> sorter = new ListSorter<Integer>();
 		sorter.insertionSort(list, comparator);
 		System.out.println(list);
@@ -118,7 +147,7 @@ public class ListSorter<T> {
 		list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
 		sorter.heapSort(list, comparator);
 		System.out.println(list);
-	
+
 		list = new ArrayList<Integer>(Arrays.asList(6, 3, 5, 8, 1, 4, 2, 7));
 		List<Integer> queue = sorter.topK(4, list, comparator);
 		System.out.println(queue);
